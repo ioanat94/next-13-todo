980660c7c8435d163de04fd28d511057a7f99ab5
Ioana Tiplea <ioanatiplea94@gmail.com>
2023-01-13 11:47:16 +0200

    Add headers

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head />
+      <head>
+        <title>Website</title>
+      </head>
       <body>
         <Header />
         {children}
