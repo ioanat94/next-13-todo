80670e65d5b6496e3643da90aa1c781762a6b042
Ioana Tiplea <ioanatiplea94@gmail.com>
2023-01-13 11:26:04 +0200

    Create Search form

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,6 +1,33 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useState } from 'react';
+
 function Search() {
-  return <div>Search</div>;
+  const [search, setSearch] = useState('');
+  const router = useRouter();
+
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch('');
+    router.push(`/search/${search}`);
+  };
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type='text'
+        value={search}
+        placeholder='Search term'
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+        type='submit'
+        className='bg-blue-500 text-white font-bold py-2 px-4 rounded-lg'
+      >
+        Search
+      </button>
+    </form>
+  );
 }
 export default Search;
